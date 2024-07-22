542da1f224f292ae2214e6f6ddbfa8e3e4cf4b0f
sbolla2 <sahitbolla27@gmail.com>
2024-07-22 10:22:19 -0400

    update cdk perms

---
+++ backend/lib/hosting/amplify.ts
@@ -0,0 +1,44 @@
+import { SecretValue } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as amplify from '@aws-cdk/aws-amplify-alpha'
+
+type AmplifyHostingProps = {
+	appName: string
+	branch: string
+	ghOwner: string
+	repo: string
+	ghTokenName: string
+}
+
+export function createAmplifyHosting(
+	scope: Construct,
+	props: AmplifyHostingProps
+) {
+	const amplifyApp = new amplify.App(scope, `${props.appName}-hosting`, {
+		appName: `${props.appName}`,
+		sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+			owner: props.ghOwner,
+			repository: props.repo,
+			oauthToken: SecretValue.secretsManager(props.ghTokenName),
+		}),
+		platform: amplify.Platform.WEB_COMPUTE,
+		autoBranchDeletion: true,
+		customRules: [
+			{
+				source: '/<*>',
+				target: '/index.html',
+				status: amplify.RedirectStatus.NOT_FOUND_REWRITE,
+			},
+		],
+		environmentVariables: {
+			myAmplifyEnv: 'frontend', //process.env.myAmplifyEnv
+		},
+    })
+
+    amplifyApp.addBranch(props.branch, {
+		stage: props.branch === 'main' ? 'PRODUCTION' : 'DEVELOPMENT',
+		branchName: props.branch,
+	})
+
+	return amplifyApp
+}
